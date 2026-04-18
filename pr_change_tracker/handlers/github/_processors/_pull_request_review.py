cf9fd584f7b1df41fdf23b4a9a6c1ce53f888f8b
Stephen Moore <stephen.moore@kraken.tech>
2026-04-18 17:56:46 +1000

    Add review information to pull request review submitted events

--- pr_change_tracker/handlers/github/_processors/_pull_request_review.py
+++ pr_change_tracker/handlers/github/_processors/_pull_request_review.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 import abc
-from collections.abc import Iterator
+import datetime
+from collections.abc import Iterator, Mapping
+from typing import ClassVar, Literal, Self
 
 import attrs
+import jsonpath
 
 from pr_change_tracker import storage
 from pr_change_tracker.handlers import github as github_handlers
@@ -14,6 +17,44 @@ from . import _common
 class PullRequestReviewEvent(abc.ABC):
     @abc.abstractmethod
     def process(self) -> None: ...
+
+
+class _Pointers:
+    review_id: ClassVar[jsonpath.JSONPointer] = jsonpath.JSONPointer("/review/id")
+    review_submitted_at: ClassVar[jsonpath.JSONPointer] = jsonpath.JSONPointer(
+        "/review/submitted_at"
+    )
+    review_commit_id: ClassVar[jsonpath.JSONPointer] = jsonpath.JSONPointer("/review/commit_id")
+    review_state: ClassVar[jsonpath.JSONPointer] = jsonpath.JSONPointer("/review/state")
+
+
+def _as_int(value: object) -> int:
+    assert isinstance(value, int)
+    return value
+
+
+@attrs.frozen
+class _Review:
+    review_id: int
+    submitted_at: datetime.datetime
+    commit_id: str
+    state: Literal["changes_requested", "approved"]
+
+    @classmethod
+    def from_data(cls, data: Mapping[str, object]) -> Self | None:
+        match state := _Pointers.review_state.resolve(data):
+            case "changes_requested" | "approved":
+                return cls(
+                    review_id=_as_int(_Pointers.review_id.resolve(data)),
+                    submitted_at=datetime.datetime.fromisoformat(
+                        str(_Pointers.review_submitted_at.resolve(data))
+                    ),
+                    commit_id=str(_Pointers.review_commit_id.resolve(data)),
+                    state=state,
+                )
+
+            case _:
+                return None
 
 
 @attrs.frozen
@@ -33,6 +74,7 @@ class _SubmittedEvent(PullRequestReviewEvent):
     _storage: storage.CommonStorage
 
     pull_request: _common.PullRequest
+    review: _Review
     timestamps: _common.Timestamps
     sender: _common.Sender
 
@@ -55,12 +97,15 @@ class PullRequestReviewProcessor:
                 )
 
             case "submitted":
-                yield _SubmittedEvent(
-                    storage=self._storage,
-                    pull_request=_common.PullRequest.from_data(incoming.body),
-                    timestamps=_common.Timestamps.from_data(incoming.body),
-                    sender=_common.Sender.from_data(incoming.body),
-                )
+                review = _Review.from_data(incoming.body)
+                if review is not None:
+                    yield _SubmittedEvent(
+                        storage=self._storage,
+                        review=review,
+                        pull_request=_common.PullRequest.from_data(incoming.body),
+                        timestamps=_common.Timestamps.from_data(incoming.body),
+                        sender=_common.Sender.from_data(incoming.body),
+                    )
 
             case "edited":
                 # Don't care for edited
--- tests/test_github/processors/test_pull_request_review.py
+++ tests/test_github/processors/test_pull_request_review.py
@@ -1,3 +1,5 @@
+import datetime
+
 import attrs
 import pytest
 from pr_change_tracker_test_driver import comparators
@@ -28,7 +30,14 @@ class PerTestLogic:
         delfick = _common.Sender(id=109301, login="delfick")
         kcollasarundell = _common.Sender(id=393998, login="kcollasarundell")
 
-    def assertFixture(self, fixture_name: str, *events: object) -> None:
+    def assertFixture(self, fixture_name: str, event: object | None, *events: object) -> None:
+        if event is None:
+            assert len(events) == 0
+            events = ()
+        else:
+            assert not any(event is None for event in events)
+            events = (event, *events)
+
         incoming = self._hook_fixtures.incoming_from_fixture(fixture_name)
         processor = github_handlers.IncomingProcessor(storage=self.storage)
         assert list(processor.process(incoming)) == list(events)
@@ -73,6 +82,12 @@ class TestPullRequestReviewEvents:
                 "submitted-approve",
                 _pull_request_review._SubmittedEvent(
                     storage=test_logic.storage,
+                    review=_pull_request_review._Review(
+                        review_id=2431221502,
+                        submitted_at=datetime.datetime.fromisoformat("2024-11-13T00:30:22Z"),
+                        commit_id="20be90fb76987ea58ad9c7698bf06658b45178d1",
+                        state="approved",
+                    ),
                     timestamps=comparators.IsInstance.using(_common.Timestamps),
                     sender=test_logic.Senders.kcollasarundell,
                     pull_request=attrs.evolve(
@@ -86,6 +101,12 @@ class TestPullRequestReviewEvents:
                 "submitted-changes_requested-collab",
                 _pull_request_review._SubmittedEvent(
                     storage=test_logic.storage,
+                    review=_pull_request_review._Review(
+                        review_id=2431185757,
+                        submitted_at=datetime.datetime.fromisoformat("2024-11-13T00:14:56Z"),
+                        commit_id="20be90fb76987ea58ad9c7698bf06658b45178d1",
+                        state="changes_requested",
+                    ),
                     timestamps=comparators.IsInstance.using(_common.Timestamps),
                     sender=test_logic.Senders.kcollasarundell,
                     pull_request=attrs.evolve(
@@ -95,27 +116,7 @@ class TestPullRequestReviewEvents:
             )
 
         def test_submitted_commented_collab(self, test_logic: PerTestLogic) -> None:
-            test_logic.assertFixture(
-                "submitted-commented-collab",
-                _pull_request_review._SubmittedEvent(
-                    storage=test_logic.storage,
-                    timestamps=comparators.IsInstance.using(_common.Timestamps),
-                    sender=test_logic.Senders.kcollasarundell,
-                    pull_request=attrs.evolve(
-                        test_logic.pull_request, pr_number=1, branch_name="test1"
-                    ),
-                ),
-            )
+            test_logic.assertFixture("submitted-commented-collab", None)
 
         def test_submitted_commented_owner(self, test_logic: PerTestLogic) -> None:
-            test_logic.assertFixture(
-                "submitted-commented-owner",
-                _pull_request_review._SubmittedEvent(
-                    storage=test_logic.storage,
-                    timestamps=comparators.IsInstance.using(_common.Timestamps),
-                    sender=test_logic.Senders.delfick,
-                    pull_request=attrs.evolve(
-                        test_logic.pull_request, pr_number=1, branch_name="test1"
-                    ),
-                ),
-            )
+            test_logic.assertFixture("submitted-commented-owner", None)
