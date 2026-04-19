e9abff0f8d867a86c5a0b0d7718dcafa7592bf1c
Stephen Moore <stephen.moore@kraken.tech>
2026-04-20 09:32:25 +1000

    Proving the point of being able to pass around a metrics object

--- pr_change_tracker/progress.py
+++ pr_change_tracker/progress.py
@@ -1,3 +1,4 @@
+import abc
 from typing import Self
 
 import attrs
@@ -6,9 +7,24 @@ import structlog
 type Logger = structlog.stdlib.BoundLogger
 
 
+class Metrics(abc.ABC):
+    @abc.abstractmethod
+    def increment(self, key: str) -> None: ...
+
+
+class NoopMetrics(Metrics):
+    """
+    More just proving the point of being able to pass around metrics than actually implementing that
+    """
+
+    def increment(self, key: str) -> None:
+        pass
+
+
 @attrs.frozen
 class Progress:
     logger: Logger
+    metrics: Metrics = attrs.field(factory=NoopMetrics)
 
     def with_bound_logger(self, **values: object) -> Self:
         return attrs.evolve(self, logger=self.logger.bind(**values))
